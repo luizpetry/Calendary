205eb8b5883b42be88fc99885c0ddc5b2b584e22
luizpetry <juniorpetry.k1@gmail.com>
2025-12-16 15:55:56 -0300

    Update header

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,7 @@ const Navigation = () => {
         <div className="flex items-center gap-2">
           <CalendarIcon className="h-5 w-5" />
           <Link to="/">
-            <span className="font-medium cursor-pointer">Calendário Minimalista</span>
+            <span className="font-medium cursor-pointer">Calendário Interativo</span>
           </Link>
         </div>
       </div>
